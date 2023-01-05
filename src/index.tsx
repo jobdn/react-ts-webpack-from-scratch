1a034bf573e83b55c99747887f6f993737fca769
Pisarev Danila <jobdan.dev@gmail.com>
2023-01-05 10:56:24 +0300

    feat: add eslint and pre commit hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(<h1>Scratch React App</h1>);
